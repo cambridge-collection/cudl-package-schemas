1166899d53d44611133df7b672bd53d5c32fd5d7
jlf44 <jlf44@cam.ac.uk>
2019-04-29 15:11:22 +0100

    Adding example data and updating script to take in a username and password

--- XML-generation-scripts/ItemJSONtoXML.py
+++ XML-generation-scripts/ItemJSONtoXML.py
@@ -10,6 +10,7 @@ import sys
 import getopt
 import re
 import ItemValidation
+import requests
 
 
 def main(argv):
@@ -19,14 +20,17 @@ def main(argv):
     id = ""
 
     try:
-        opts, args = getopt.getopt(argv, "i:o:v:", ["input=", "output=", "validation="])
+        opts, args = getopt.getopt(argv, "i:o:v:up", ["input=", "output=", "validation=", "username=", "password="])
 
-        if len(argv) != 6:
-            raise getopt.GetoptError("incorrect number of arguments")
+        if len(argv) not in [6, 10]:
+            raise getopt.GetoptError("Incorrect number of arguments")
 
-    except getopt.GetoptError:
+    except getopt.GetoptError as e:
+        print(e.msg)
         print('Usage is: ItemJSONtoXML.py -i <inputfile> -o <outputfile> --validation True or')
-        print('          ItemJSONtoXML.py -i <inputURL> -o <outputfile> --validation True')
+        print('          ItemJSONtoXML.py -i <inputURL> -o <outputfile> --validation True or')
+        print('          ItemJSONtoXML.py -i <inputURL> -o <outputfile> '
+              '--validation True --username <urlusername> --password <userpass>')
         sys.exit()
     for opt, arg in opts:
         if opt in ("-i", "--input"):
@@ -35,10 +39,22 @@ def main(argv):
             output_file = arg
         elif opt in ("-v", "--validation"):
             validation = arg
+        elif opt in ("-u", "--username"):
+            username = arg
+        elif opt in ("-p", "--password"):
+            password = arg
 
     if _is_url(input_file):
-        url = urllib.request.urlopen(input_file)
-        json_data = url.read()
+
+        if not username or not password:
+            r = requests.get(input_file)
+        else:
+            r = requests.get(input_file, auth=(username, password))
+        json_data = r.text
+
+        if r.status_code != 200:
+            raise Exception("Problem getting JSON from URL, error code: " + str(r.status_code))
+
         # Get id from file name.
         a = urllib.parse.urlparse(input_file)
         id = os.path.basename(a.path).replace('.json', '')
@@ -106,6 +122,10 @@ def main(argv):
     xmlstr = ET.tostring(item, encoding='UTF-8', method='xml')
     dom = minidom.parseString(xmlstr)
     xml = dom.toprettyxml(indent="   ", encoding='UTF-8')
+
+    # create dir if it does not exist
+    if not os.path.exists(os.path.dirname(output_file)):
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
     with open(output_file, "wb") as f:
         f.write(xml)
