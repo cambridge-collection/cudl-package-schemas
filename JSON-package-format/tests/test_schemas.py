7d3e931cffb05b10e1675825b0a7fb117db69ab8
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-23 17:07:42 +0100

    Setup tests for the package item formats
    Note that the item schemas are currently not valid, so these test are
    failing.

--- JSON-package-format/tests/test_schemas.py
+++ JSON-package-format/tests/test_schemas.py
@@ -1,9 +1,29 @@
 from schema_testing import BaseDatatypeTest
 
 
-class TestCollection(BaseDatatypeTest):
+class CUDLSchemaTest(BaseDatatypeTest):
+    schema_base_uri = 'https://schemas.cudl.lib.cam.ac.uk/package/v1/'
+
+
+class TestCollection(CUDLSchemaTest):
     data_type = 'collection'
     expected_valid_count = 2
     expected_invalid_count = 2
 
-    schema_base_uri = 'https://schemas.cudl.lib.cam.ac.uk/package/v1/'
+
+class TestItem(CUDLSchemaTest):
+    data_type = 'item'
+    expected_valid_count = 2
+    expected_invalid_count = 1
+
+
+class TestCUDLItem(CUDLSchemaTest):
+    data_type = 'cudl-item'
+    expected_valid_count = 0
+    expected_invalid_count = 0
+
+
+class TestMUDLItem(CUDLSchemaTest):
+    data_type = 'mudl-item'
+    expected_valid_count = 1
+    expected_invalid_count = 0
