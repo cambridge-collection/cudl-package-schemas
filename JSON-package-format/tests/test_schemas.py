f3109f10709e36cfd9c3d76c6a7d0e812a462f18
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-24 11:30:55 +0100

    Test schemas against strict draft 07 meta schema

--- JSON-package-format/tests/test_schemas.py
+++ JSON-package-format/tests/test_schemas.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import json
+
+import pytest
+import jsonschema
+import json5
+
 from schema_testing import BaseDatatypeTest
 
 
@@ -27,3 +34,21 @@ class TestMUDLItem(CUDLSchemaTest):
     data_type = 'mudl-item'
     expected_valid_count = 1
     expected_invalid_count = 0
+
+
+@pytest.fixture(scope='module')
+def draft07_strict_meta_schema():
+    with open(Path(__file__).parents[2] / 'support' / 'draft-07-strict-schema.json5') as f:
+        return json5.load(f)
+
+
+@pytest.mark.parametrize(
+    'schema_path',
+    [str(p) for p in Path(__file__).parents[1].glob('schemas/*.json')],
+    ids=lambda p: Path(p).name)
+def test_schemas_match_strict_meta_schema(schema_path,
+                                          draft07_strict_meta_schema):
+    with open(schema_path) as f:
+        schema = json.load(f)
+
+    jsonschema.validate(schema, draft07_strict_meta_schema)
