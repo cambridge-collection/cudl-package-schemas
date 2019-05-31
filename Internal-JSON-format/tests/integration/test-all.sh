7ea87dcced5e1eb3bf77b278883fc056ebb40ca1
Hal Blackburn <hwtb2@cam.ac.uk>
2019-05-31 14:37:00 +0100

    Allow specifying AJV options in integration/test-all.sh

--- Internal-JSON-format/tests/integration/test-all.sh
+++ Internal-JSON-format/tests/integration/test-all.sh
@@ -5,6 +5,7 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 ITEM_SCHEMA="${ITEM_SCHEMA:-$DIR/../../schemas/item.json}"
 ITEM_JSON_DIR="${ITEM_JSON_DIR:-}"
 ITEM_SCHEMA_PATCH="${ITEM_SCHEMA_PATCH:-$DIR/allow-minor-deviations.patch.json}"
+AJV_OPTIONS="${AJV_OPTIONS:---json-pointers --verbose}"
 
 if [[ "$ITEM_JSON_DIR" == "" || ! -d "$ITEM_JSON_DIR" ]] ; then
     echo "Error: \$ITEM_JSON_DIR is not a directory: $ITEM_JSON_DIR"
@@ -22,5 +23,5 @@ else
 fi
 
 find "$ITEM_JSON_DIR" -type f -name '*.json' \
-    | env SHELL=/bin/sh parallel -X ajv test --json-pointers --verbose -s "$VALIDATION_SCHEMA" "-d={}" --valid 2>&1 \
+    | env SHELL=/bin/sh parallel -X ajv test $AJV_OPTIONS -s "$VALIDATION_SCHEMA" "-d={}" --valid 2>&1 \
     | grep -vP 'passed test$'
